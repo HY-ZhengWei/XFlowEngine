276b9005a1f2fa4977563620605f272fe2a0d962
HY-ZhengWei <hy.zhengwei@qq.com>
2018-05-17 17:52:59 +0800

    1.0.0

--- src/org/hy/xflow/engine/config/InitConfig.java
+++ src/org/hy/xflow/engine/config/InitConfig.java
@@ -2,9 +2,7 @@ package org.hy.xflow.engine.config;
 
 import java.util.List;
 
-import org.hy.common.Help;
 import org.hy.common.app.Param;
-import org.hy.common.thread.ThreadPool;
 import org.hy.common.xml.XJava;
 import org.hy.common.xml.plugins.AppInitConfig;
 
@@ -51,12 +49,11 @@ public final class InitConfig extends AppInitConfig
             {
                 this.init("sys.Config.xml");
                 this.init("startup.Config.xml");
-                this.init((List<Param>)XJava.getObject("StartupConfig") ,Help.getClassPath(this));
+                this.init((List<Param>)XJava.getObject("StartupConfig"));
                 this.init(((Param)XJava.getObject("XFlow_RootPackageName")).getValue());
                 
                 if ( i_IsStartJobs )
                 {
-                    init_TPool();
                     this.init("job.Config.xml");
                 }
             }
@@ -66,25 +63,6 @@ public final class InitConfig extends AppInitConfig
                 exce.printStackTrace();
             }
         }
-    }
-    
-    
-    
-    private void init_TPool()
-    {
-        ThreadPool.setMaxThread(    this.getIntConfig("TPool_MaxThread"));
-        ThreadPool.setMinThread(    this.getIntConfig("TPool_MinThread"));
-        ThreadPool.setMinIdleThread(this.getIntConfig("TPool_MinIdleThread"));
-        ThreadPool.setIntervalTime( this.getIntConfig("TPool_IntervalTime"));
-        ThreadPool.setIdleTimeKill( this.getIntConfig("TPool_IdleTimeKill"));
-        ThreadPool.setWatch(        Boolean.parseBoolean(XJava.getParam("TPool_IsWatch").getValue()));
-    }
-    
-    
-    
-    private int getIntConfig(String i_XJavaID)
-    {
-        return Integer.parseInt(XJava.getParam(i_XJavaID).getValue());
     }
     
 }
