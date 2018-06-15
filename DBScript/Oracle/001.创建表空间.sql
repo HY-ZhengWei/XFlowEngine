4ec4ba13160e90ca55ffb4386ff935b0b69b1ff0
HY-ZhengWei <hy.zhengwei@qq.com>
2018-06-15 12:32:24 +0800

    1.0.0

--- DBScript/Oracle/001.创建表空间.sql
+++ DBScript/Oracle/001.创建表空间.sql
@@ -1,8 +1,8 @@
 -- 数据表空间
 CREATE TABLESPACE XFLOW_DATA
          DATAFILE 'D:\Software\oracle\oradata\orcl\XFLOW_DATA01.DBF' 
-             SIZE 209715200 
-       AUTOEXTEND ON NEXT 104857600 
+             SIZE 200M 
+       AUTOEXTEND ON NEXT 1M 
           MAXSIZE UNLIMITED
           LOGGING
            ONLINE;
@@ -12,8 +12,8 @@ CREATE TABLESPACE XFLOW_DATA
 -- 索引表空间
 CREATE TABLESPACE XFLOW_IDX
          DATAFILE 'D:\Software\oracle\oradata\orcl\XFLOW_IDX01.DBF' 
-             SIZE 209715200 
-       AUTOEXTEND ON NEXT 104857600 
+             SIZE 200M 
+       AUTOEXTEND ON NEXT 1M 
           MAXSIZE UNLIMITED
           LOGGING
            ONLINE;
