39062a4055707a9529e95de6c1a93e9ddd131494
HY-ZhengWei <HY.ZhengWei@qq.com>
2018-11-29 18:13:24 +0800

    1.0.3

---
+++ test/org/hy/xflow/engine/junit/S001_XX/db/流程模板v3.0.0/1.活动.sql
@@ -0,0 +1,439 @@
+ INSERT  INTO TActivityInfo
+       (
+        activityID
+       ,activityCode
+       ,templateID
+       ,activityName
+       ,activityTypeID
+       ,x
+       ,y
+       ,z
+       ,height
+       ,width
+       ,iconURL
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'A001-V3'
+       ,'选型申请'
+       ,'T001-V3'
+       ,'选型申请'
+       ,'AT000'
+       ,0
+       ,0
+       ,0
+       ,64
+       ,64
+       ,'http://'
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityInfo
+       (
+        activityID
+       ,activityCode
+       ,templateID
+       ,activityName
+       ,activityTypeID
+       ,x
+       ,y
+       ,z
+       ,height
+       ,width
+       ,iconURL
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'A001-1-V3'
+       ,'审批选型'
+       ,'T001-V3'
+       ,'审批选型'
+       ,'AT001'
+       ,0
+       ,0
+       ,0
+       ,64
+       ,64
+       ,'http://'
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityInfo
+       (
+        activityID
+       ,activityCode
+       ,templateID
+       ,activityName
+       ,activityTypeID
+       ,x
+       ,y
+       ,z
+       ,height
+       ,width
+       ,iconURL
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'A002-V3'
+       ,'选型受理'
+       ,'T001-V3'
+       ,'选型受理'
+       ,'AT001'
+       ,0
+       ,0
+       ,0
+       ,64
+       ,64
+       ,'http://'
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityInfo
+       (
+        activityID
+       ,activityCode
+       ,templateID
+       ,activityName
+       ,activityTypeID
+       ,x
+       ,y
+       ,z
+       ,height
+       ,width
+       ,iconURL
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'A003-V3'
+       ,'选型评审'
+       ,'T001-V3'
+       ,'选型评审'
+       ,'AT001'
+       ,0
+       ,0
+       ,0
+       ,64
+       ,64
+       ,'http://'
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+       
+INSERT  INTO TActivityInfo
+       (
+        activityID
+       ,activityCode
+       ,templateID
+       ,activityName
+       ,activityTypeID
+       ,x
+       ,y
+       ,z
+       ,height
+       ,width
+       ,iconURL
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'A004-V3'
+       ,'单人选型'
+       ,'T001-V3'
+       ,'单人选型'
+       ,'AT001'
+       ,0
+       ,0
+       ,0
+       ,64
+       ,64
+       ,'http://'
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+       
+INSERT  INTO TActivityInfo
+       (
+        activityID
+       ,activityCode
+       ,templateID
+       ,activityName
+       ,activityTypeID
+       ,x
+       ,y
+       ,z
+       ,height
+       ,width
+       ,iconURL
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'A005-V3'
+       ,'选型分包'
+       ,'T001-V3'
+       ,'选型分包'
+       ,'AT001'
+       ,0
+       ,0
+       ,0
+       ,64
+       ,64
+       ,'http://'
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityInfo
+       (
+        activityID
+       ,activityCode
+       ,templateID
+       ,activityName
+       ,activityTypeID
+       ,x
+       ,y
+       ,z
+       ,height
+       ,width
+       ,iconURL
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'A006-V3'
+       ,'多人选型'
+       ,'T001-V3'
+       ,'多人选型'
+       ,'AT001'
+       ,0
+       ,0
+       ,0
+       ,64
+       ,64
+       ,'http://'
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityInfo
+       (
+        activityID
+       ,activityCode
+       ,templateID
+       ,activityName
+       ,activityTypeID
+       ,x
+       ,y
+       ,z
+       ,height
+       ,width
+       ,iconURL
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'A007-V3'
+       ,'选型汇总'
+       ,'T001-V3'
+       ,'选型汇总'
+       ,'AT001'
+       ,0
+       ,0
+       ,0
+       ,64
+       ,64
+       ,'http://'
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityInfo
+       (
+        activityID
+       ,activityCode
+       ,templateID
+       ,activityName
+       ,activityTypeID
+       ,x
+       ,y
+       ,z
+       ,height
+       ,width
+       ,iconURL
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'A008-V3'
+       ,'选型结果确认'
+       ,'T001-V3'
+       ,'选型结果确认'
+       ,'AT001'
+       ,0
+       ,0
+       ,0
+       ,64
+       ,64
+       ,'http://'
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityInfo
+       (
+        activityID
+       ,activityCode
+       ,templateID
+       ,activityName
+       ,activityTypeID
+       ,x
+       ,y
+       ,z
+       ,height
+       ,width
+       ,iconURL
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'A009-V3'
+       ,'选型完成'
+       ,'T001-V3'
+       ,'选型完成'
+       ,'AT999'
+       ,0
+       ,0
+       ,0
+       ,64
+       ,64
+       ,'http://'
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
---
+++ test/org/hy/xflow/engine/junit/S001_XX/db/流程模板v3.0.0/2.路由.sql
@@ -0,0 +1,837 @@
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR000-V3'
+       ,'选型申请-审批选型'
+       ,'转向审批选型'
+       ,'T001-V3'
+       ,'RT001'
+       ,'A001-V3'
+       ,'A001-1-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR000-V3-01'
+       ,'选型申请-单人选型'
+       ,'转向单人选型'
+       ,'T001-V3'
+       ,'RT001'
+       ,'A001-V3'
+       ,'A004-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR001-V3'
+       ,'审批选型-选型受理'
+       ,'转向选型受理'
+       ,'T001-V3'
+       ,'RT001'
+       ,'A001-1-V3'
+       ,'A002-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR001-1-V3'
+       ,'审批选型-选型申请'
+       ,'审批未通过'
+       ,'T001-V3'
+       ,'RT020'
+       ,'A001-1-V3'
+       ,'A001-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR002-V3'
+       ,'选型受理-单人选型'
+       ,'受理后转向单人选型'
+       ,'T001-V3'
+       ,'RT001'
+       ,'A002-V3'
+       ,'A004-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR003-V3'
+       ,'选型受理-选型分包'
+       ,'受理后转向选型分包'
+       ,'T001-V3'
+       ,'RT001'
+       ,'A002-V3'
+       ,'A005-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR004-V3'
+       ,'选型受理-选型评审'
+       ,'受理后转向评审'
+       ,'T001-V3'
+       ,'RT001'
+       ,'A002-V3'
+       ,'A003-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR005-V3'
+       ,'选型受理-选型申请'
+       ,'受理未通过'
+       ,'T001-V3'
+       ,'RT020'
+       ,'A002-V3'
+       ,'A001-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR006-V3'
+       ,'单人选型-选型结果确认'
+       ,'单人选型转向选型结果确认'
+       ,'T001-V3'
+       ,'RT001'
+       ,'A004-V3'
+       ,'A008-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR007-V3'
+       ,'选型评审-单人选型'
+       ,'评审后转向单人选型'
+       ,'T001-V3'
+       ,'RT001'
+       ,'A003-V3'
+       ,'A004-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+      
+      
+      
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR008-V3'
+       ,'选型评审-选型分包'
+       ,'评审后转向选型分包'
+       ,'T001-V3'
+       ,'RT001'
+       ,'A003-V3'
+       ,'A005-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+      
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR009-V3'
+       ,'选型评审-选型申请'
+       ,'评审未通过'
+       ,'T001-V3'
+       ,'RT020'
+       ,'A003-V3'
+       ,'A001-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR010-V3'
+       ,'选型分包-多人选型'
+       ,'选型分包转多人选型'
+       ,'T001-V3'
+       ,'RT002'
+       ,'A005-V3'
+       ,'A006-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+ INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR011-V3'
+       ,'多人选型-选型汇总'
+       ,'多人选型转选型汇总'
+       ,'T001-V3'
+       ,'RT003'
+       ,'A006-V3'
+       ,'A007-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR012-V3'
+       ,'选型汇总-选型结果确认'
+       ,'选型汇总转选型结果确认'
+       ,'T001-V3'
+       ,'RT001'
+       ,'A007-V3'
+       ,'A008-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR013-V3'
+       ,'选型结果确认-选型完成'
+       ,'选型结果确认转选型完成'
+       ,'T001-V3'
+       ,'RT999'
+       ,'A008-V3'
+       ,'A009-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR014-V3'
+       ,'选型结果确认-单人选型'
+       ,'选型结果确认未通过转单人选型'
+       ,'T001-V3'
+       ,'RT020'
+       ,'A008-V3'
+       ,'A004-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR015-V3'
+       ,'选型结果确认-选型分包'
+       ,'选型结果确认未通过转选型分包'
+       ,'T001-V3'
+       ,'RT020'
+       ,'A008-V3'
+       ,'A005-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
+       
+       
+       
+       
+INSERT  INTO TActivityRoute
+       (
+        activityRouteID
+       ,activityRouteCode
+       ,activityRouteName
+       ,templateID
+       ,routeTypeID
+       ,activityID
+       ,nextActivityID
+       ,conditionXJavaID
+       ,conditionMethod
+       ,conditionValue
+       ,conditionVType
+       ,infoComment
+       ,createrID
+       ,creater
+       ,createTime
+       ,lastUserID
+       ,lastUser
+       ,lastTime
+       )
+VALUES (
+        'AR021-V3'
+       ,'选型分包-选型结果确认'
+       ,'临时的，直接跳汇总'
+       ,'T001-V3'
+       ,'RT001'
+       ,'A005-V3'
+       ,'A008-V3'
+       ,''
+       ,''
+       ,''
+       ,''
+       ,''
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       ,'8a81b2aa4e4865b4014e535c6c900115'
+       ,'郑伟'
+       ,'2018-11-29 01:01:01'
+       );
