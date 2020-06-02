0d36a099c7852bcfe89d6b2a4d8226802476a0ec
HY-ZhengWei <hy.zhengwei@qq.com>
2020-06-02 14:03:18 +0800

    1.0.9
    1. 修复：MySQL脚本连接字符串时当做数字四则运算了

--- Maven.Deploy.Win.XFlowEngine.bat
+++ Maven.Deploy.Win.XFlowEngine.bat
@@ -1,2 +1,2 @@
-start mvn deploy:deploy-file -Dfile=hy.xflow.engine.jar                              -DpomFile=./src/META-INF/maven/org/hy/xflow/engine/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/nexus/content/repositories/thirdparty
-start mvn deploy:deploy-file -Dfile=hy.xflow.engine-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/xflow/engine/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/nexus/content/repositories/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.xflow.engine.jar                              -DpomFile=./src/META-INF/maven/org/hy/xflow/engine/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.xflow.engine-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/xflow/engine/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
