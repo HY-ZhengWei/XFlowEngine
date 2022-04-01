c6051ba96af78832ad97e4ed657ff8a34a4ea532
ZhengWei <HY.ZhengWei@qq.com>
2022-04-01 16:12:59 +0800

    1.1.1

---
+++ test/org/hy/xflow/engine/junit/清理实例的脚本.sql
@@ -0,0 +1,11 @@
+TRUNCATE TABLE TFlowInfo;
+TRUNCATE TABLE TFlowInfo_History;
+
+TRUNCATE TABLE TProcess;
+TRUNCATE TABLE TProcess_History;
+
+TRUNCATE TABLE TProcessParticipants;
+TRUNCATE TABLE TProcessParticipants_History;
+
+TRUNCATE TABLE TFlowLastOperator;
+TRUNCATE TABLE TFlowFutureOperator;
